3fdf2b2a3303ffcf5f8795de088c18538623624d
Glitch <glitch.txs@gmail.com>
2023-09-07 01:07:27 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+
+import { WagmiConfig } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+
+// 1. Get projectId
+const projectId = 'cdbd18f9f96172be74c3e351ce99b908'
+
+// 2. Create wagmiConfig
+const chains = [mainnet, goerli]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, appName: 'Web3Modal' })
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains,   themeVariables: {
+  '--w3m-border-radius-master': '1px'
+} })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  )
 }
